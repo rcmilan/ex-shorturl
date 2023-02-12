5a61ed5a0846bd474df1f4299d77d03e7bc2f10e
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-02-12 09:16:52 -0300

    buscando url

--- ShortUrlApi/Program.cs
+++ ShortUrlApi/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
 using ShortUrlApi.DTOs;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Redis
+var multiplexer = ConnectionMultiplexer.Connect(new ConfigurationOptions { EndPoints = { builder.Configuration.GetConnectionString("Redis") } });
+builder.Services.AddSingleton<IConnectionMultiplexer>(multiplexer);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,18 +25,30 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/g", () =>
+app.MapGet("/g/{url}", async ([FromServices] IConnectionMultiplexer redis, string url) =>
 {
-    var result = Results.Redirect("https://www.google.com", true, true);
+    var cacheDb = redis.GetDatabase();
+
+    string decodedUrl = Uri.UnescapeDataString(url);
+
+    var redisValue = await cacheDb.StringGetAsync(decodedUrl);
+
+    var result = Results.Redirect(redisValue.ToString(), true, true);
 
     return result;
 })
 .WithName("GetURL")
 .WithOpenApi();
 
-app.MapPost("/p", (PostShortUrlInput input) =>
+app.MapPost("/p", async ([FromServices] IConnectionMultiplexer redis, PostShortUrlInput input) =>
 {
+    var cacheDb = redis.GetDatabase();
+
     var urlOutput = new PostShortUrlOutput(input.Origin);
+
+    var expiration = input.Expiration.Subtract(DateTime.Now);
+
+    await cacheDb.StringSetAsync(urlOutput.NewUrl, input.Origin, expiration);
 
     return urlOutput;
 })
